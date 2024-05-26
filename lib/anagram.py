5680b7d5d164b03946365fe7d9a41198de5fb975
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-05-26 10:19:11 +0300

    pp

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted(word)
+
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word) == self.word_letters]
